6d68884525a467b5009253550a5fc6778fefbd30
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-02 11:32:34 +0330

    Remove linters error.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Button from './Button';
+import Button from './Buttons';
 import calculate from './logic/calculate';
 
 const Calculator = () => {
--- src/components/logic/calculate.js
+++ src/components/logic/calculate.js
@@ -105,7 +105,7 @@ export default function calculate(obj, buttonName) {
     if (obj.total && !obj.next) {
       return { ...obj, operation: buttonName };
     }
-    
+
     if (!obj.total) {
       return { total: 0, operation: buttonName };
     }
